63d37abddd0354d7a41f1e1a1403ff5b4e3c4163
BAANG <chcelestino@gmail.com>
2019-07-25 00:15:25 -0700

    Create character card component

---
+++ src/components/CharacterCard/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.css';
+import { url } from 'inspector';
+
+const CharacterCard = ({ id, name, image, handleUserPick }) => (
+    <div>
+        <div
+        className = "card"
+        key = {id}
+        data-id = {id}
+        name = {name}
+        image = {{ backgroundImage: `url(${image}`}}
+        onClick = {handleUserPick}>
+        </div>
+    </div>
+)
