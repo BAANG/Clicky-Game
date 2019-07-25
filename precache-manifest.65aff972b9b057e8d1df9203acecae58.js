7c356904f5f3dac22e06d901c6e8986b9ef3d9f7
BAANG <chcelestino@gmail.com>
2019-07-25 14:38:01 -0700

    Updates

---
+++ precache-manifest.65aff972b9b057e8d1df9203acecae58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd7f7e2940b30aa3dae318b309a9f4d9",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "d32f386cc2f1f02ecfe6",
+    "url": "/Clicky-Game/static/css/main.a53d06a9.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/Clicky-Game/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "d32f386cc2f1f02ecfe6",
+    "url": "/Clicky-Game/static/js/main.d8671940.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Clicky-Game/static/media/logo.5d5d9eef.svg"
+  }
+]);
